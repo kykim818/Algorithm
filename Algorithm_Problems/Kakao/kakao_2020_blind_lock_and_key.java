b2b9f18a9fecd0cf34d3733740a22ce0d3f8d2ed
Kim Ki Young <kykim818@naver.com>
2020-10-18 23:28:13 +0900

    카카오 2019 블라인드 코딩문제_자물쇠와열쇠

--- Algorithm_Problems/Kakao/kakao_2020_blind_lock_and_key.java
+++ Algorithm_Problems/Kakao/kakao_2020_blind_lock_and_key.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 
 public class kakao_blind_2020_lock_and_key {
-	static int n;
+	static int n,m;
+	static boolean check;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] key = {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
 		int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
 		boolean answer = false;
-		// 수정
+		
 		// idea : 완탐  
 		// M이 최대가 N이랑 같을때 
 		
@@ -16,20 +17,94 @@ public class kakao_blind_2020_lock_and_key {
 		//방향별로 n-1칸씩 최대 이동할수잇음 그이상이동하면 열쇠칸을 벗어남 
 		// 00 기준
 		n = key.length;
+		m = lock.length;
+		int cnt = 0;
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < m; j++) {
+				if(lock[i][j] == 0) cnt++;
+			}
+		}
+		
+		check = false;
 		int[][] keycase = new int[n][n];
-		for(int i = 0; i < 4 ; i++) { // 0 그대로 // 1일떄 90 2 180 3 270
+		L : for(int i = 0; i < 4 ; i++) { // 0 그대로 // 1일떄 90 2 180 3 270
 			keycase = rotateKey(key,i);
-			
+			//위 , 좌상 우상 체크
+			for(int k = 0 ; k <= m-1; k++) {
+				for(int l = 0; l <= m-1; l++) {
+					answer = matching(keycase,lock,k,l,0,cnt);
+					if(answer == true) break L; 
+				}
+				for(int r = 0; r <= m-1; r++) {
+					answer = matching(keycase,lock,k,r,1,cnt);
+					if(answer == true) break L; 
+				}
+			}
+			//아래 , 
+			for(int k = 0 ; k <= m-1; k++) {
+				for(int l = 0; l <= m-1; l++) {
+					answer = matching(keycase,lock,k,l,2,cnt);
+					if(answer == true) break L; 
+				}
+				for(int r = 0; r <= m-1; r++) {
+					answer = matching(keycase,lock,k,r,3,cnt);
+					if(answer == true) break L; 
+				}
+			}
 		}
-//		keycase = rotateKey(key,3);
-		for(int i = 0; i < n; i++) {
-			System.out.println(Arrays.toString(keycase[i]));
-		}
+		System.out.println(answer);
 		
 		
 //		key는 M x M(3 ≤ M ≤ 20, M은 자연수)크기 2차원 배열입니다.
 //		lock은 N x N(3 ≤ N ≤ 20, N은 자연수)크기 2차원 배열입니다.
 //		M은 항상 N 이하입니다.
+	}
+	private static boolean matching(int[][] keycase, int[][] lock, int tb, int lr, int dir, int cnt) {
+		boolean match = false;
+		int tbDir = 0;
+		int lrDir = 0;
+		switch(dir){
+			case 0: // 상 좌
+				tbDir = -1;
+				lrDir = -1;
+				break;
+			case 1: // 상 우
+				tbDir = -1;
+				lrDir = 1;
+				break;
+			case 2: // 하 좌
+				tbDir = 1;
+				lrDir = -1;
+				break;
+			case 3: // 하 우
+				tbDir = 1;
+				lrDir = 1;
+				break;
+		}
+		// keycase를 이동시키면서 매칭 
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				int mov_row = i + tbDir * tb;
+				int mov_col = j + lrDir * lr;
+				if(isIn(mov_row,mov_col)) {
+					if(keycase[i][j] == 1 && lock[mov_row][mov_col] == 1) {
+						return false;
+					}
+					if(keycase[i][j] == 0 && lock[mov_row][mov_col] == 0) {
+						return false;
+					}
+					if(keycase[i][j] == 1 && lock[mov_row][mov_col] == 0) {
+						cnt--;
+					}
+				}
+			}
+		}
+		if(cnt == 0) match = true;
+		return match;
+	}
+	private static boolean isIn(int mov_row, int mov_col) {
+		// TODO Auto-generated method stub
+		return mov_row >= 0 && mov_row < m && mov_col >=0 && mov_col < m;
 	}
 	private static int[][] rotateKey(int[][] key, int i) {
 		int[][] ret = new int[n][n];
