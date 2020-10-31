95ebcd5dbeeedcde27c2aa7625f5381db65480f4
Kim Ki Young <kykim818@naver.com>
2020-10-31 17:50:41 +0900

    N으로 표현(해결)

--- Algorithm_Problems/Programmers/Programmers_divide_N.java
+++ Algorithm_Problems/Programmers/Programmers_divide_N.java
@@ -1,31 +1,54 @@
+import java.util.ArrayList;
 
 public class programmers_divde_N {
-	// 잘못된풀이
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int N = 5;
-		int number = 12;
+		int N = 2;
+		int number = 11;
 		int answer = -1;
-		int[] memo= new int[number+1];
-		if(N == 1) {
-			if(number <= 8)
-				answer = number;
-		}else {
-			memo[0] = 2;
-			memo[1] = 2;
-			for(int i = 2; i <= number; i++) {
-				if(i == N) {
-					memo[i] = 1;
-				}else if(i % N == 0) {
-					memo[i] = i/N * memo[N];
-				}else if(i < N) {
-					memo[i] = i+1;
-				}else {
-					memo[i] = i/N * memo[N]+ memo[i % N];
+		int[] val = new int[9];
+		// 개수 1~8개  8보다크면 -1
+		val[0] = 1;
+		val[1] = N;
+		for(int i = 2; i < 9; i++) {
+			val[i] = val[i-1] * 10 + N; 
+		}
+		// 보통 배열선언 int[] a = new int[크기];
+		ArrayList<Integer>[] list = new ArrayList[9];
+		for(int i = 0; i < 9 ; i++) {
+			list[i] = new ArrayList<>();
+			list[i].add(val[i]);
+		}
+		int tmp = 0;
+		// 모든결과에 대해 순차적으로 구해나가야 빠트리지 않고 계산이됨 
+		// 1개를 사용했을때 -> 2개를 사용햇을때 -> 3개를 사용했을때 이런식으로 
+		// 모든 결과를 구해보자
+		for(int i = 2; i < 9; i++) { 
+			// 2번사용부터 8번사용까지 구할것
+			// j번 사용 + s번 사용 리스트 이런식으로 구할것임 
+			for(int j = 1; j < i; j++) { 
+			// N을 J번 사용한 숫자들의 리스트
+				for(int j1 = 0; j1 < list[j].size(); j1++) {
+					tmp = list[j].get(j1);
+					for(int s = 0; s < list[i-j].size(); s++) {
+						int s1 = list[i-j].get(s);
+						if(!list[i].contains(tmp+s1)) list[i].add(tmp+s1);
+						if(tmp > s1 && !list[i].contains(tmp-s1)) list[i].add(tmp-s1);
+						if(!list[i].contains(tmp*s1)) list[i].add(tmp*s1);
+						if(s1 != 0 && !list[i].contains(tmp / s1)) list[i].add(tmp/s1);
+					}
 				}
 			}
-			answer = memo[number];
 		}
+		L1 : for(int i = 1; i< 9; i++) {
+				for(int j = 0 ; j < list[i].size(); j++) {
+					if(list[i].get(j) == number) {
+						answer = i;
+						break L1;
+					}
+				}
+			}
 		System.out.println(answer);
 	}
 
