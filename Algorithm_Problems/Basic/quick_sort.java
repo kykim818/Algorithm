475440d3babd8615d16d51582e9365284576ccec
Kim Ki Young <kykim818@naver.com>
2020-11-16 22:57:40 +0900

    퀵 정렬

--- Algorithm_Problems/Basic/quick_sort.java
+++ Algorithm_Problems/Basic/quick_sort.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 public class quick_sort {
-
+	//같은수 들으가면 무한 루프 .. 수정해야함
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {0,4,5,2,6,1,3,3};
+		int[] arr = {0,4,5,2,6,1,3,8};
 		quickSort(arr,0,arr.length-1);
 		System.out.println(Arrays.toString(arr));
 	}
@@ -22,11 +22,11 @@ public class quick_sort {
 	private static int partition(int[] arr, int low, int high) {
 		// TODO Auto-generated method stub
 		int pivot = arr[(low+high)/2];
-		while(low <= high) {
+		while(low < high) {
 			while(arr[low] < pivot) low++;
 			while(arr[high] > pivot) high--;
 			
-			if(low <= high) {
+			if(low < high) {
 				int tmp = arr[low];
 				arr[low] = arr[high];
 				arr[high] = tmp;
@@ -34,4 +34,5 @@ public class quick_sort {
 		}
 		return high;
 	}
+
 }
