e3860fcaa649b2d696fcc1cce5a51efa7d89aee8
KiYoung <kykim818@naver.com>
2021-02-12 19:48:52 +0900

    단계12. 백트랙킹

--- Algorithm_Problems/BaekJoon/단계별로 풀어보기/백트래킹/baekjoon_9663.java
+++ Algorithm_Problems/BaekJoon/단계별로 풀어보기/백트래킹/baekjoon_9663.java
@@ -1,28 +1,65 @@
-import java.util.*;
 import java.io.*;
 
 public class baekjoon_9663 {
     // 9663 N-Queen
-    static int answer; 
+    static int answer;
+    static int N; 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
         boolean[][] check = new boolean[N][N];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                nQueen(i,j,check);
-            }
-        }        
+        nQueen(0,check);
         System.out.println(answer);
 
     }
-    private static void nQueen(int row,int col,boolean[][] check){
-        if(row == check.length-1){
+    private static void nQueen(int row,boolean[][] check){
+        if(row == check.length){
             answer++;
             return;
         }
-            
+        int mov_row = 0;
+        int mov_col = 0;
+        for(int col = 0; col < N; col++){
+            mov_row = row;
+            mov_col = col;
+            if(isOk(mov_row,mov_col,check)){
+               check[mov_row][mov_col] = true;
+               nQueen(row+1, check); 
+               check[mov_row][mov_col] = false;
+            }
+        }
+    }
 
+    private static boolean isOk(int row,int col,boolean[][] check){
+        int mov_row = row-1;
+        int mov_col = col-1;
+        while(mov_row >= 0 && mov_col >= 0){
+            if(check[mov_row][mov_col] == true){
+                return false;
+            }
+            mov_row--;
+            mov_col--;
+        }
+
+        mov_row = row-1;
+        mov_col = col;
+        while(mov_row >= 0){
+            if(check[mov_row][mov_col] == true){
+                return false;
+            }
+            mov_row--;
+        }
+
+        mov_row = row-1;
+        mov_col = col+1;
+        while(mov_row >= 0 && mov_col < N){
+            if(check[mov_row][mov_col] == true){
+                return false;
+            }
+            mov_row--;
+            mov_col++;
+        }
+        return true;
     }
 }
