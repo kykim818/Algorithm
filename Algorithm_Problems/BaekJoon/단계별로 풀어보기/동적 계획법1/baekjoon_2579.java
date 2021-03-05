37c9e850bab3d797ec8df92bf13130ff6f945d97
KiYoung <kykim818@naver.com>
2021-03-05 21:06:18 +0900

    단계14. 동적 계획법1

--- Algorithm_Problems/BaekJoon/단계별로 풀어보기/동적 계획법1/baekjoon_2579.java
+++ Algorithm_Problems/BaekJoon/단계별로 풀어보기/동적 계획법1/baekjoon_2579.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.io.*;
 
 public class baekjoon_2579 {
@@ -11,16 +10,21 @@ public class baekjoon_2579 {
             score[i] = Integer.parseInt(br.readLine());
         }
         int[][] memo = new int[N][2];
-        DP(N,memo,score);
-        
-    }
-    public static int[] DP(int N, int[][] memo, int[] score){
-        if(N == 0){
-            memo[N][0] = score[0];
-            memo[N][1] = score[0];
-            return memo[N];
+        memo[0][0] = score[0];
+        memo[0][1] = score[0];
+        if(N >= 2){
+            memo[1][0] = memo[0][0] + score[1];
+            memo[1][1] = score[1];
         }
 
-        memo[N][0] = DP(N-1)
+        for(int i = 2; i < N; i++){
+            memo[i][0] = memo[i-1][1] + score[i];
+            memo[i][1] = Math.max(memo[i-2][0], memo[i-2][1]) + score[i];
+        }
+        
+        System.out.println(Math.max(memo[N-1][0], memo[N-1][1]));
+        // 0 이전계단 밟기
+        // 1 이전계단 안밟고 두계단 전꺼 밟기 
     }
+    
 }
