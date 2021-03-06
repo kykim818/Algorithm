b56b80a595874ee0cebc1bff5e4b428e6c05b629
KiYoung <kykim818@naver.com>
2021-03-06 23:59:12 +0900

    단계14. 동적 계획법1

---
+++ Algorithm_Problems/BaekJoon/단계별로 풀어보기/동적 계획법1/baekjoon_1463.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+
+public class baekjoon_1463 {
+    // 1463 1로 만들기
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+    }
+    
+}
